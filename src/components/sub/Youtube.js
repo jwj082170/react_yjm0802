234a682bac47f39b754d3be23f69b457489620c6
tubi55 <tubi55@nate.com>
2022-08-10 12:47:06 +0900

    유튜브 데이터 출력

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,7 +1,36 @@
 import Layout from '../common/Layout';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function Youtube() {
-	return <Layout name={'Youtube'}>Youtube</Layout>;
+	const [Vids, setVids] = useState([]);
+
+	useEffect(() => {
+		const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
+		const playlist = 'PLHtvRFLN5v-VD95TBpr5Dh2zguWCjjmMG';
+		const num = 6;
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
+		axios.get(url).then((json) => {
+			setVids(json.data.items);
+		});
+	}, []);
+
+	return (
+		<Layout name={'Youtube'}>
+			{Vids.map((vid, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{vid.snippet.title}</h2>
+						<p>{vid.snippet.description}</p>
+						<img
+							src={vid.snippet.thumbnails.standard.url}
+						/>
+						<span>{vid.snippet.publishedAt}</span>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
 
 export default Youtube;
