471d40f944b313fa57eda160ee95144d15f40c44
tubi55 <tubi55@nate.com>
2022-08-10 15:19:42 +0900

    useRef로 참조한 객체를 확인할때 유의할 점

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -11,7 +11,17 @@ function Youtube() {
 	const [Open, setOpen] = useState(false);
 	const [Index, setIndex] = useState(0);
 
+	//이곳에 가상돔을 제어하는 코드를 집어넣으면 동작되지 않음
+	//이곳은 리턴문이 실행되서 가상돔의 화면에 출력(mount)되기전이기 때문
+
 	useEffect(() => {
+		//해당 공간은 return문이 실행되서 가상돔에 화면에 출력(mount)된 이후 호출되는 공간
+		//이곳에 icon참조 객체에 담겨있는 참조된 웹폰트 아이콘 확인이 불가
+		// --> 이유 이곳에 Vids값이 만들어지고 state에 담기면 그 다음사이클에서 Vids를 통해서 article요소가 반복적으로 만들어지고
+		// 참고객체에 담은 아이콘 요소는 바로 article요소 안쪽에 있기 때문
+		// 스테이트에 의해서 반복으로 동적으로 만들어지는 요소를 참고하기 위해서는 단지 useEffect안쪽에서 참고객체를 호출하는 것이 아닌
+		// 해당 스테이트에 값이 담기는 순간이 바로 동적으로 article요소가 만들어지는 순간이기 때문에.
+		// 해당 스테이트를 의존성배열로 등록한 useEffect안쪽에서 참조객체를 확인할 수 있음
 		const key = 'AIzaSyC77Pd__ju0Wqx_Umc-IuW7Cn2mWi_HVsk';
 		const playlist = 'PLHtvRFLN5v-VD95TBpr5Dh2zguWCjjmMG';
 		const num = 6;
