4cdb86cf6ea29ac6332b6ee2098f8774d9e84343
tubi55 <tubi55@nate.com>
2022-08-10 14:18:40 +0900

    팝업생성시 스크롤 기능 비활성화

--- src/components/common/Popup.js
+++ src/components/common/Popup.js
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+
 function Popup(props) {
+	useEffect(() => {
+		document.body.style.overflowY = 'hidden';
+
+		return () => {
+			document.body.style.overflowY = 'auto';
+		};
+	}, []);
+
 	return (
 		<aside className='popup'>
 			<div className='con'></div>
