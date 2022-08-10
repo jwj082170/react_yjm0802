e34210cde1ba9ef373f522b443315c8d08cdc712
tubi55 <tubi55@nate.com>
2022-08-10 10:11:17 +0900

    기본 라우터 링크 설정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,5 +1,29 @@
+import { Link, NavLink } from 'react-router-dom';
+
 function Header() {
-	return <div>Header</div>;
+	const style = { color: 'hotpink' };
+	return (
+		<header>
+			<h1>
+				<Link exact to='/'>
+					LOGO
+				</Link>
+			</h1>
+
+			<ul id='gnb'>
+				<li>
+					<NavLink to='/department' activeStyle={style}>
+						Department
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/youtube' activeStyle={style}>
+						Youtube
+					</NavLink>
+				</li>
+			</ul>
+		</header>
+	);
 }
 
 export default Header;
