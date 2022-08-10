f40c6999e16298b025c847f797edef1bd244c1d1
tubi55 <tubi55@nate.com>
2022-08-10 16:09:29 +0900

    기본 카카오 지도 화면에 출력

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,9 +1,21 @@
 import Layout from '../common/Layout';
+import { useRef, useEffect } from 'react';
 
 function Location() {
+	const { kakao } = window;
+	const container = useRef(null);
+	const option = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667),
+		level: 3,
+	};
+
+	useEffect(() => {
+		new kakao.maps.Map(container.current, option);
+	}, []);
+
 	return (
 		<Layout name={'Location'}>
-			<div id='map'></div>
+			<div id='map' ref={container}></div>
 		</Layout>
 	);
 }
