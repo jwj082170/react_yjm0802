37b1f3d78307c0d9f391d3d5301af911027e591f
tubi55 <tubi55@nate.com>
2022-08-10 15:48:41 +0900

    kakao 지도가 들어갈 프레임 설정

--- src/components/sub/Location.js
+++ src/components/sub/Location.js
@@ -1,7 +1,11 @@
 import Layout from '../common/Layout';
 
 function Location() {
-	return <Layout name={'Location'}>location</Layout>;
+	return (
+		<Layout name={'Location'}>
+			<div id='map'></div>
+		</Layout>
+	);
 }
 
 export default Location;
